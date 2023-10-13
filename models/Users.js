c2deeef360f1794dd794ad68214f807b082debd6
Sergey Kuran <kuran.serg@gmail.com>
2023-10-13 20:44:27 +0300

    Create Users.js

---
+++ models/Users.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const { handleSaveError, validatorsAtUpdate } = require("../models/hooks");
+const {
+  nameValidationRegex,
+  passwordValidationRegex,
+  emailValidationRegex,
+} = require("../utils/validation/constans/regex");
+
+const schemaUser = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+schemaUser.post("save", handleSaveError);
+schemaUser.pre("findOneAndUpdate", validatorsAtUpdate);
+schemaUser.post("findOneAndUpdate", handleSaveError);
+
+const userSingupSchema = Joi.object({
+  username: Joi.string().pattern(nameValidationRegex).required(),
+  email: Joi.string().pattern(emailValidationRegex).required(),
+  password: Joi.string().min(6).pattern(passwordValidationRegex).required(),
+});
+
+const userSinginSchema = Joi.object({
+  email: Joi.string().pattern(emailValidationRegex).required(),
+  password: Joi.string().required(),
+});
+
+const User = model("user", schemaUser);
+
+module.exports = {
+  User,
+  userSingupSchema,
+  userSinginSchema,
+};
