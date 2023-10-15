ebac24d9063583d71410da8417aee4eedb3c3611
Sergey Kuran <kuran.serg@gmail.com>
2023-10-15 22:25:58 +0300

    Update

--- middlewares/validateBodyAuth.js
+++ middlewares/validateBodyAuth.js
@@ -5,7 +5,6 @@ const validateBodyAuth = (schema) => {
     const { error } = schema.validate(req.body);
 
     if (error) {
-      //   const missingField = error.details[0].context.key;
       return next(HttpError(400, `${error.message}`));
     }
 
