e5dae5bcfcbd58bd87a359b80f4f7f5fb1953b88
Sergey Kuran <kuran.serg@gmail.com>
2023-10-24 22:06:45 +0300

    Fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const { globalError } = require("./middlewares/globalError");
 const { errorNotFound } = require("./middlewares/errorNotFound");
+
 require("dotenv").config();
 
 const contactsRouter = require("./routes/contacts");
@@ -19,7 +20,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", authRouter);
-app.use("/api", contactsRouter);
+app.use("/api/contacts", contactsRouter);
+
 app.use(errorNotFound);
 app.use(globalError);
 
